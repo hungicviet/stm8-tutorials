1be0fcf54e76d1927ebbb773dbd356572bc1afd6
hungicviet <hungnt@icviet.com>
2016-03-15 23:36:30 +0700

    2 tutorial: gpio and exti

---
+++ exti/main.c
@@ -0,0 +1,123 @@
+/**
+  ******************************************************************************
+  * @file    GPIO_Toggle\main.c
+  * @author  MCD Application Team
+  * @version  V2.2.0
+  * @date     30-September-2014
+  * @brief   This file contains the main function for GPIO Toggle example.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm8s.h"
+#include "main.h"
+/**
+  * @addtogroup GPIO_Toggle
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Evalboard I/Os configuration */
+
+
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+void Delay (uint16_t nCount);
+static void gpio_config(void);
+
+/* Private functions ---------------------------------------------------------*/
+/* Public functions ----------------------------------------------------------*/
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+void main(void)
+{
+  gpio_config(); 
+  EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOD, EXTI_SENSITIVITY_FALL_ONLY);
+  EXTI_SetTLISensitivity(EXTI_TLISENSITIVITY_FALL_ONLY);
+  
+  enableInterrupts();
+  
+
+
+
+  while (1)
+  {
+
+  }
+  
+
+}
+
+/**
+  * @brief Delay
+  * @param nCount
+  * @retval None
+  */
+void Delay(uint16_t nCount)
+{
+  /* Decrement nCount value */
+  while (nCount != 0)
+  {
+    nCount--;
+  }
+}
+
+static void gpio_config(void)
+{
+  GPIO_Init(LED_PORT, (GPIO_Pin_TypeDef)LED_PIN, GPIO_MODE_OUT_PP_HIGH_FAST);
+  GPIO_WriteLow(LED_PORT, (GPIO_Pin_TypeDef)LED_PIN);
+  GPIO_Init(BUTTON_PORT, (GPIO_Pin_TypeDef)BUTTON_PIN, GPIO_MODE_IN_FL_IT);
+}
+
+
+#ifdef USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param file: pointer to the source file name
+  * @param line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ exti/main.h
@@ -0,0 +1,21 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Evalboard I/Os configuration */
+
+#define LED_PORT        (GPIOD)
+#define LED_PIN         (GPIO_PIN_3)
+#define BUTTON_PORT     (GPIOD)
+#define BUTTON_PIN      (GPIO_PIN_6) 
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Public functions ----------------------------------------------------------*/
+
+#endif /* __MAIN_H */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
